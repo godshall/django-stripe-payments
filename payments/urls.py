ef5f594f05ef66fcaeefd3b56cb4611c1b7888f7
Michael Godshall <michaelgodshall@gmail.com>
2013-03-11 16:43:29 -0700

    Replaced direct_to_template with TemplateView

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib.auth.decorators import login_required
 
@@ -18,38 +18,33 @@ urlpatterns = patterns(
     
     url(
         r"^subscribe/$",
-        login_required(direct_to_template),
+        login_required(TemplateView.as_view(template_name="payments/subscribe.html")),
         {
-            "template": "payments/subscribe.html",
             "extra_context": {"form": PlanForm}
         },
         name="payments_subscribe"
     ),
     url(
         r"^change/card/$",
-        login_required(direct_to_template),
-        {"template": "payments/change_card.html"},
+        login_required(TemplateView.as_view(template_name="payments/change_card.html")),
         name="payments_change_card"
     ),
     url(
         r"^change/plan/$",
-        login_required(direct_to_template),
+        login_required(TemplateView.as_view(template_name="payments/change_plan.html")),
         {
-            "template": "payments/change_plan.html",
             "extra_context": {"form": PlanForm}
         },
         name="payments_change_plan"
     ),
     url(
         r"^cancel/$",
-        login_required(direct_to_template),
-        {"template": "payments/cancel.html"},
+        login_required(TemplateView.as_view(template_name="payments/cancel.html")),
         name="payments_cancel"
     ),
     url(
         r"^history/$",
-        login_required(direct_to_template),
-        {"template": "payments/history.html"},
+        login_required(TemplateView.as_view(template_name="payments/history.html")),
         name="payments_history"
     ),
 )
